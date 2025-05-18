598961de91a7dd047db6134b9e547a66e1128a21
krystoliz <109867611+krystoliz@users.noreply.github.com>
2025-05-18 13:49:40 +0700

    feat: using promise-based fetch

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-fetch('https://api.example.com/data')
-    .then((response) => response.json())
-    .then((data) => console.log(data))
+fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => console.log(data))
+    .then(data => console.log(data.name))
+    .then(data => console.log(data.weight))
+    .then(data => console.log(data.id))
     .catch((error) => console.error('Error:', error));
