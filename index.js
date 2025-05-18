9237fed9f64f4fa36743e595e74c2f44c820a51d
krystoliz <109867611+krystoliz@users.noreply.github.com>
2025-05-18 19:09:13 +0700

    feat: fetch pokemon by name

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => console.log(data))
-    .then(data => console.log(data.name))
-    .then(data => console.log(data.weight))
-    .then(data => console.log(data.id))
-    .catch((error) => console.error('Error:', error));
+fetchPokemon();
+
+async function fetchPokemon(url) {
+  try{
+    const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+    if(!response.ok){
+        throw new Error("Could not fetch resource");
+    }
+
+    const data = await response.json();
+    console.log(data);
+  }
+  catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
